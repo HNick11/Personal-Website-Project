c189ba1b909476b9ef19ce3591bef60efeda1487
HNick11 <73151171+HNick11@users.noreply.github.com>
2021-12-22 20:05:50 -0600

    Styling and Grid placement
    Created a grid for the nav boxes at the top of the page. Made the button's hover colors match the gradient header of the main page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import './index.css';
-import ReactDOM from 'react-dom';
 import test from './test.png';
 import NavCard from './components/NavCard';
 import TextCard from './components/TextCard';
@@ -8,15 +7,28 @@ import TextCard from './components/TextCard';
 function App() {
   return (
     <div class="grid place-items-center h-screen">
-        <h1 className="text-3xl font-bold underline">
+            
+    <h1 class="w-full text-3xl text-white font-bold bg-gradient-to-r from-sky-600 via-purple-600 to-pink-600">
       Nicholas Heleta's Portfolio Website
     </h1>
 
-    {/*Start of card here*/}
+    <div class="px-2">
+    <div class="flex -mx-2">
+      <div class="w-1/3 px-2">
+      <NavCard title="Projects" text="Click here to see some of my projects!"/>
+      </div>
+      <div class="w-1/3 px-2">
+        <NavCard title="About me" text="Click here to learn more about me!"/>
+      </div>
+      <div class="w-1/3 px-2">
+      <NavCard title="Classes" text="Click here to see some of the classes I've taken!"/>
+      </div>
+      
+    </div>
+    </div>
+  
     <TextCard photo={test} text="Sample dice text box" tag1="Tag 1" tag2="Tag 2" tag3="Tag 3"/>
   
-    <NavCard title="Test tile" text="Sample text box"/>
-
     </div>
   );
 }
--- my-app/src/components/NavCard.js
+++ my-app/src/components/NavCard.js
@@ -3,7 +3,7 @@ import '../index.css';
 function NavCard(props){
     return (
     <div>
-    <a href="#" class="group block bg-gray-300 max-w-xs mx-auto rounded-lg p-6 bg-whitering-1 ring-gray-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+    <a href="#" class="group block bg-gray-300 max-w-xs mx-auto rounded-lg p-6 bg-whitering-1 ring-gray-900/5 shadow-lg space-y-3 hover:bg-gradient-to-r from-sky-600 via-purple-600 to-pink-600 hover:ring-sky-500">
         <div class="flex items-center space-x-3">
 
         <h3 class="text-gray-900 group-hover:text-white text-sm font-semibold">{props.title}</h3>
