f4c3d788be43308200f54d27ef0e7ce148d1960d
HNick11 <73151171+HNick11@users.noreply.github.com>
2021-12-22 19:04:10 -0600

    Created another type of card
    Created a smaller card, im thinking of using these to navigate to other pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,8 @@ function App() {
         <h1 className="text-3xl font-bold underline">
       Nicholas Heleta's Portfolio Website
     </h1>
+
+    {/*Start of card here*/}
     <div class="max-w-sm rounded overflow-hidden shadow-lg border-2 border-blue-600">
     <img class="w-full" src={test} alt="Test photo"></img>
     <div class="px-6 py-4">
@@ -21,6 +23,15 @@ function App() {
     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Tag 3</span>
   </div>
 </div>
+{/*Start of project card here */}
+<a href="#" class="group block bg-gray-300 max-w-xs mx-auto rounded-lg p-6 bg-whitering-1 ring-gray-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+  <div class="flex items-center space-x-3">
+
+    <h3 class="text-gray-900 group-hover:text-white text-sm font-semibold">New project</h3>
+  </div>
+  <p class="text-gray-500 group-hover:text-white text-sm">Create a new project from a variety of starting templates.</p>
+</a>
+
     </div>
   );
 }
